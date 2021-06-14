ffbaa00a1b044d17cac05e3b1e646990a2639d1c
shpank12 <pankaj@spliceglobal.com>
2021-06-14 22:50:10 +0530

    update config

--- localmingle/settings.py
+++ localmingle/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 MEDIA_URL = '/media/'
-MEDIA_ROOT = location('/public/media')
+MEDIA_ROOT = location('public/media')
 STATIC_ROOT = location('public/static')
 
 STATIC_URL = '/static/'
